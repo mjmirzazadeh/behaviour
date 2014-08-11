07ec2c06d5491f0fb50d587945215df6d1582d84
MJ <mj@mirzazadeh.net>
2014-08-11 17:42:00 +1000

    More user model tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140811042922) do
+ActiveRecord::Schema.define(version: 20140811073741) do
 
   create_table "case_managers", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140811042922) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "firstname"
   end
 
   add_index "case_managers", ["email"], name: "index_case_managers_on_email", unique: true, using: :btree
--- spec/models/case_manager_spec.rb
+++ spec/models/case_manager_spec.rb
@@ -4,16 +4,29 @@ describe CaseManager do
   it "instanciates with a valid email and password" do
     user = CaseManager.create(email:"a@a.com", password:"12345678")
     expect(user).to be_valid
+    expect(user.save).to be true
   end
 
-  it "doesn't instanciate with an unvalid email" do
+  it "doesn't instanciate with an unvalid email or password" do
     user = CaseManager.create(email:"a", password:"12345678")
     expect(user).not_to be_valid
-  end
 
-  it "doesn't instanciate with an unvalid password" do
     user = CaseManager.new(email:"a@a.com", password:"1234567")
     expect(user).not_to be_valid
   end
-  
+
+  it "should not save with duplicated email" do
+    user = CaseManager.create(email:"a@a.com", password:"12345678")
+    user.save
+    user2 = CaseManager.create(email:"a@a.com", password:"12345678")
+    expect(user2).not_to be_valid
+  end
+
+  it "should have firstname and lastname" do
+    user = CaseManager.create(email:"a", password:"12345678")
+    expect(user.respond_to?(:firstname)).to be true
+    expect(user.respond_to?(:lastname)).to be true
+  end
+
+
 end
